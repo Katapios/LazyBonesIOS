d6d0f1e01e73b288267b2c734427e93a56af483a
Dennis Ryumin <d.ryumin@sigma-it.ru>
2025-07-10 09:07:50 +0300

    Initial Commit

---
+++ LazyBones/LazyBonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyBonesApp.swift
+//  LazyBones
+//
+//  Created by Денис Рюмин on 10.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct LazyBonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
